b5a232dbbb44c22b962fed061bdb210911af4af0
louiseeo <louisenicole.madriaga@lorma.edu>
2026-03-10 12:58:27 +0800

    new

--- MovieRegistrationMenu.java
+++ MovieRegistrationMenu.java
@@ -1,6 +1,7 @@
+import java.io.*;
 import java.util.*;
 
-public class Activity3Midterms {
+public class MovieRegistrationMenu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
@@ -36,7 +37,9 @@ public class Activity3Midterms {
                     // Sort
                     break;
                 case 6:
-                    // List
+                    // List / Display items
+                    displayList();
+
                     break;
                 case 0:
                     System.out.println("Exiting program...");
@@ -47,5 +50,38 @@ public class Activity3Midterms {
 
         } while (choice != 0);
         sc.close();
+    }
+
+    // Method 1: Adding
+    // Method 2: Search
+    // Method 3: Edit
+    // Method 4: Delete
+    // Method 5: Sort
+
+    // Method 6: List/Display elements in the array
+    public static void displayList() {
+        String filename = "movies.txt";
+
+        ArrayList<String> movie = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                movie.add(line);
+            }
+
+            // condition if there are no movies on the list
+            if (movie.isEmpty()) {
+                System.out.println("No movies available.");
+            } else {
+                for (String m : movie) {
+                    System.out.println(m);
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Error reading the file.");
+        }
     }
 }

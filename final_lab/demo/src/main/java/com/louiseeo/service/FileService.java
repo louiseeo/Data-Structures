b22fe0ed52883c1bea3d27c5c05ff516b5496adf
unknown <louisenicole.madriaga@lorma.edu>
2026-05-12 12:26:53 +0800

    lab upd

--- final_lab/demo/src/main/java/com/louiseeo/service/FileService.java
+++ final_lab/demo/src/main/java/com/louiseeo/service/FileService.java
@@ -1,11 +1,9 @@
 package com.louiseeo.service;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.lang.reflect.Type;
@@ -14,7 +12,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.louiseeo.model.GameResult;
-import com.louiseeo.model.Player;
 import com.louiseeo.model.WordPair;
 
 public class FileService {
@@ -23,10 +20,9 @@ public class FileService {
      * Loads the word bank from the specified file.
      *
      * @param filename : the path to the file containing pairs
-     * @param out      : the output stream to send error messages to the client
      * @return a list of pairs loaded from the file
      */
-    public static List<WordPair> loadWordbank(String filename, PrintWriter out) {
+    public static List<WordPair> loadWordbank(String filename) {
         List<WordPair> pairs = new ArrayList<>();
         File file = new File(filename);
         if (!file.exists()) {
@@ -48,11 +44,29 @@ public class FileService {
         return pairs;
     }
 
+    /**
+     * Saves a game result to the game history file.
+     * The result is converted into JSON format and appended
+     * to the specified file.
+     *
+     * @param filename the path to the game history file
+     * @param result   the game result object to save
+     */
     public static void saveGameHistory(String filename, GameResult result) {
-        FileWriter fw = new FileWriter(filename)
-        // hint: use GsonBuilder with pretty printing
-        // hint: use FileWriter with append mode -> new FileWriter(filename, true)
-        // just toJson the result and write it
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        try (FileWriter fw = new FileWriter(filename, true)) {
+
+            String json = gson.toJson(result);
+
+            fw.write(json);
+            fw.write(System.lineSeparator());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
